b3a0b5806adc5eafdeb449211188a8325970edca
Gomida <145982843+Gomida05@users.noreply.github.com>
2025-05-05 15:10:13 +0100

    Update App opener.py

--- App opener.py
+++ App opener.py
@@ -22,34 +22,3 @@ class MyAppOpener:
             print("App opened successfully")
         except Exception as e:
             print("Error: Failed to open the application or couldn't find an app named ", app_name)
-
-MyAppOpener().open_app("GitHub Desktopclient")
-
-
-# while True:
-#     x = input('Enter the app name you want to open or type "ls" to list the apps name or "exit" to exit: ').lower()
-#     seen = set()
-
-#     Applist = get_installed_software()
-
-#     unique_software_list = [software for software in Applist if software['name'] not in seen and not seen.add(software['name'])]
-#     g = 0
-#     if x == 'ls' or x == "dir":
-#         for software in unique_software_list:
-#             g +=1
-#             if software['name'] == "":
-#                 continue
-#             else:
-#                 print("%d\t%s" % (g, software['name']))
-#             #print(software['name'])
-#     elif x in [software['name'].lower() for software in unique_software_list]:
-#         try:
-#             open(x)
-#             print("App founded successfully")
-#             break
-#         except Exception as e:
-#             print("Error: Failed to open the application or couldn't find an app named ", x)
-#     elif x == 'exit' :
-#         exit()
-#     else:
-#         print("sorry I couldn't find an app named ", x)
